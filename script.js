5f1eade032449ec41208c9da0eb40b556f497451
Nova Amiko <beckhv2@gmail.com>
2023-05-11 13:50:01 -0800

    added html template with links; added code signature to js

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+Title: Color Picker
+Description: Allows a user to pick or enter custom colors and view related 
+colors.
+Last Updated: May 11, 2023
+Developer: Alexander Beck
+Email: beckhv2@gmail.com
+Github: https://github.com/bexcoding
+*/
+
