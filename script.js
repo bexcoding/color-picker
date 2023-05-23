6259b704184b5aa9e210b37882f59d192725843f
Alexander Beck <beckhv2@gmail.com>
2023-05-22 16:06:03 -0800

    finished some more of the hsl function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 Title: Color Picker
 Description: Allows a user to pick or enter custom colors and view related 
 colors.
-Last Updated: May 19, 2023
+Last Updated: May 22, 2023
 Developer: Alexander Beck
 Email: beckhv2@gmail.com
 Github: https://github.com/bexcoding
@@ -38,25 +38,39 @@ function hslToRgb(hslValue) {
         const hPrime = hslValue.hue / 60;
         const x = c * (1 - Math.abs(hPrime % 2 - 1));
         const m = hslValue.lightness - (c / 2);
+        let r1 = 0;
+        let g1 = 0;
+        let b1 = 0;
         switch(hPrime) {
             case (hPrime <= 0 && hPrime <= 1):
-            
+                r1 = c;
+                g1 = x;
                 break;
-            case ():
-            
+            case (hPrime <= 1 && hPrime <= 2):
+                r1 = x;
+                g1 = c;
                 break;           
-            case ():
-            
+            case (hPrime <= 2 && hPrime <= 3):
+                g1 = c;
+                b1 = x;
                 break;    
-            case ():
-            
+            case (hPrime <= 3 && hPrime <= 4):
+                g1 = x;
+                b1 = c; 
                 break;
-            case ():
-            
+            case (hPrime <= 4 && hPrime <= 5):
+                r1 = x;
+                b1 = c;
                 break;
             default:
+                r1 = c;
+                b1 = x;
                 break;
         }
+        const red = (r1 + m) * 255;
+        const green = (g1 + m) * 255;
+        const blue = (b1 + m) * 255;
+        return Rgb(red, green, blue);
     } else {
         console.log("HSL object is invalid");
     }
