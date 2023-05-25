f00d6b38a3e089bd36e00c8f02289e583d41a7e7
Alexander Beck <beckhv2@gmail.com>
2023-05-24 16:42:43 -0800

    added initial canvas for color picker

--- script.js
+++ script.js
@@ -2,11 +2,29 @@
 Title: Color Picker
 Description: Allows a user to pick or enter custom colors and view related 
 colors.
-Last Updated: May 23, 2023
+Last Updated: May 24, 2023
 Developer: Alexander Beck
 Email: beckhv2@gmail.com
 Github: https://github.com/bexcoding
 */
+
+function createCanvas() {
+    var colorCanvas = document.getElementById('color-canvas');
+    var ColorCtx = colorCanvas.getContext('2d');  // This create a 2D context for the canvas
+    var color = 'rgba(0, 0, 255, 1)';
+    let gradientH = ColorCtx.createLinearGradient(0, 0, ColorCtx.canvas.width, 0);
+    gradientH.addColorStop(0, 'rgba(255, 255, 255, 0');
+    gradientH.addColorStop(0.9, color);
+    ColorCtx.fillStyle = gradientH;
+    ColorCtx.fillRect(0, 0, ColorCtx.canvas.width, ColorCtx.canvas.height);   
+    // Create a Vertical Gradient(white to black)
+    let gradientV = ColorCtx.createLinearGradient(0, 0, 0, 300);
+    gradientV.addColorStop(0, 'rgba(255, 255, 255, 0.2');
+    gradientV.addColorStop(0.5, 'rgba(0, 0, 0, 1');
+    ColorCtx.fillStyle = gradientV;
+    ColorCtx.fillRect(0, 0, ColorCtx.canvas.width, ColorCtx.canvas.height);
+
+}
 
 function updateRelated() {
     resetDisplay(document.getElementById('related-color-display'));
